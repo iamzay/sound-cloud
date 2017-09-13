d018969e4e6fae17b067a2def0019241b7bbbd26
Unknown <zayhust@gmail.com>
2017-09-13 15:09:13 +0800

    实现下拉加载

--- src/containers/Songs.js
+++ src/containers/Songs.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import SongCard from '../components/SongCard.js';
+import Spinner from '../components/Spinner.js';
 
 import { getCurrentPlayingId } from '../utils/songUtils.js';
 import { fetchSongsIfNeeded } from '../actions/playlistActions.js';
 import { playSong, toggleIsPlaying } from '../actions/playerActions.js';
 
 class Songs extends Component {
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
   componentDidMount() {
     const { playlist, playlists, dispatch } = this.props;
     if (!(playlist in playlists) || playlists[playlist].items.length === 0) {
       dispatch(fetchSongsIfNeeded(playlist));
     }
+
+    window.addEventListener('scroll', this.handleScroll);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -24,6 +31,20 @@ class Songs extends Component {
       ) {
         dispatch(fetchSongsIfNeeded(nextProps.playlist));
       }
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const { playlist, dispatch } = this.props;
+    if (
+      window.innerHeight + window.scrollY >
+      document.body.offsetHeight - 200
+    ) {
+      dispatch(fetchSongsIfNeeded(playlist));
     }
   }
 
@@ -45,6 +66,47 @@ class Songs extends Component {
     }
   }
 
+  renderSongs(items) {
+    const { playlists, entities, player } = this.props;
+    const { isPlaying } = player;
+    const { songs, users } = entities;
+    const currentPlayingId = getCurrentPlayingId(player, playlists);
+
+    const chunk = 5;
+    const songRows = [];
+    for (let i = 0; i < items.length; i += chunk) {
+      /* 渲染每行的5首歌 */
+      const songRow = items.slice(i, i + chunk).map((songId, index) => {
+        const song = songs[songId];
+        const user = users[song.user_id];
+        const isActive = currentPlayingId === song.id;
+        return (
+          <SongCard
+            isActive={isActive}
+            isPlaying={isPlaying}
+            togglePlaying={this.togglePlaying.bind(this, isActive, index)}
+            song={song}
+            user={user}
+            key={index}
+          />
+        );
+      });
+
+      console.log(i);
+      /* 添加一行 */
+      songRows.push(
+        <div
+          className="song-row"
+          key={'song-row-' + (Math.floor(i / chunk) + 1)}
+        >
+          {songRow}
+        </div>
+      );
+    }
+
+    return songRows;
+  }
+
   render() {
     const { playlist, playlists, entities, player } = this.props;
     const { isPlaying } = player;
@@ -52,26 +114,12 @@ class Songs extends Component {
     const currentPlayingId = getCurrentPlayingId(player, playlists);
     const playlistInfo = playlists[playlist];
     const items = playlistInfo ? playlistInfo.items : [];
+    const isFetching = !playlistInfo || playlistInfo.isFetching;
 
     return (
       <div className="container">
-        <div className="songs-container">
-          {items.map((songId, index) => {
-            const song = songs[songId];
-            const user = users[song.user_id];
-            const isActive = currentPlayingId === song.id;
-            return (
-              <SongCard
-                isActive={isActive}
-                isPlaying={isPlaying}
-                togglePlaying={this.togglePlaying.bind(this, isActive, index)}
-                song={song}
-                user={user}
-                key={index}
-              />
-            );
-          })}
-        </div>
+        <div className="songs-container">{this.renderSongs(items)}</div>
+        {isFetching ? <Spinner /> : null}
       </div>
     );
   }
